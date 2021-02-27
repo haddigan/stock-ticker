58158d2e4afabe218081f0b5d0bbc5f929e4c7ea
Dan <code@haddigan.email>
2021-02-27 12:09:43 -0500

    make data returned from useSearch frendlier

--- client/src/components/StockSelector.js
+++ client/src/components/StockSelector.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDebounce, useSearch } from "../hooks";
 
 export const StockSelector = () => {
-  const [searchQuery, setSearchQuery] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
   const debouncedQuery = useDebounce(searchQuery, 500);
 
   const [searchResults] = useSearch(debouncedQuery);
@@ -15,7 +15,12 @@ export const StockSelector = () => {
         value={searchQuery}
         placeholder="Search by company name or symbol"
       />
-      {searchResults && <p>{JSON.stringify(searchResults)}</p>}
+      <ul>
+        {searchResults &&
+          searchResults.map(({ name, symbol }) => {
+            return <li key={name}>{symbol}</li>;
+          })}
+      </ul>
     </div>
   );
 };
--- client/src/hooks/useSearch.js
+++ client/src/hooks/useSearch.js
@@ -2,6 +2,17 @@ import { useState, useEffect } from "react";
 
 const URI = `${process.env.REACT_APP_API_URI}`;
 
+const SYMBOL_KEY = "1. symbol";
+const NAME_KEY = "2. name";
+
+const formatResults = (results) => {
+  return results.map((result) => {
+    const symbol = result[SYMBOL_KEY];
+    const name = result[NAME_KEY];
+    return { symbol, name };
+  });
+};
+
 export const useSearch = (term) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -16,7 +27,9 @@ export const useSearch = (term) => {
       try {
         const result = await fetch(`${URI}/search/${term}`);
         const json = await result.json();
-        setResponse(json);
+        const { bestMatches } = json;
+        const formattedResults = formatResults(bestMatches);
+        setResponse(formattedResults);
         setIsLoading(false);
         setIsIdle(true);
       } catch (err) {
