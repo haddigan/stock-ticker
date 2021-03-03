8ba3f5de00c0dfe503f2d1a84784fd51dd1ba166
Dan <code@haddigan.email>
2021-03-03 12:22:38 -0500

    add test todos

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+it("renders a heading", () => {
   render(<App />);
   const headingText = screen.getByText(/stock comparison/i);
   expect(headingText).toBeInTheDocument();
 });
+
+it.todo("displays a stock when one is chosen from the dropdown");
+
+it.todo("shows up to three pinned stocks");
+
+it.todo("removes a stock from the pinned stocks");
+
+it.todo("renders error states");
---
+++ client/src/components/StockDetails/StockDetails.test.tsx
@@ -0,0 +1,8 @@
+import { StockDetails } from "./StockDetails";
+
+it.todo("renders stock information");
+it.todo("renders a close button");
+it.todo("fires a handler when close button is clicked");
+it.todo("shows a loading state");
+it.todo("shows an error state");
+test.todo("error state allows retry");
